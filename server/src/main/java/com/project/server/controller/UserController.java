6826bd1624044e776b85e41817cd52963413bc86
KayChicken <kainov.dmitry370@yandex.ru>
2024-04-19 13:19:07 +0300

    m

--- server/src/main/java/com/project/server/controller/UserController.java
+++ server/src/main/java/com/project/server/controller/UserController.java
@@ -1,12 +1,33 @@
 package com.project.server.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.server.model.Token;
+import com.project.server.model.User;
+import com.project.server.service.JwtService;
 
 import lombok.AllArgsConstructor;
 
-
 @Controller
 @AllArgsConstructor
+@RequestMapping("/auth")
 public class UserController {
-    
+    private JwtService service;
+
+    @PostMapping
+    public ResponseEntity isAuth(@RequestBody Token token) {
+        String username = service.extractUserName(token.getToken());
+        if (username == null || username.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Пользователь не найден");
+        }
+
+        return ResponseEntity.ok(username);
+    }
 }
---
+++ server/src/main/java/com/project/server/model/Token.java
@@ -0,0 +1,11 @@
+package com.project.server.model;
+
+public class Token {
+    private String token;
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
